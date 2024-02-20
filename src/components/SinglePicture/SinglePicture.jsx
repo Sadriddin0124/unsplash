6bc3c937c6e6929e0b744fe402eb7d4804863bdf
Your Name <you@example.com>
2024-02-20 21:16:58 +0500

    4th commit

--- src/components/SinglePicture/SinglePicture.jsx
+++ src/components/SinglePicture/SinglePicture.jsx
@@ -4,40 +4,43 @@ import { IoCloseSharp } from "react-icons/io5";
 import { GrNext, GrPrevious } from "react-icons/gr";
 import {  FaHeart, FaPlus } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
-import Pictures from "../Pictures/Pictures";
+import SinglePhotos from "./Singlephotos/SinglePhotos";
 
-const SinglePicture = ({index, photos, singlePictureStatus, setSinglePictureStatus}) => {
+const SinglePicture = ({index, photos, singlePictureStatus, setSinglePictureStatus, downloadImage, setPropIndex}) => {
     let single = photos[index]
+    console.log(single);
   return (
-    <div onClick={()=>setSinglePictureStatus(false)} className={singlePictureStatus ? "singlepicture" : "hidden"}>
-      <div className="single__card">
+    <div  className={singlePictureStatus ? "singlepicture flex" : "hidden"}>
+      <div className="blackcurtain" onClick={()=>setSinglePictureStatus(false)}></div>
         <button onClick={()=>setSinglePictureStatus(false)} className="single_close">
             <IoCloseSharp/>
         </button>
+      <div className="single__card">
         <div className="single__card-top">
             <div className="single__card-left">
                 <img src={single?.user?.profile_image?.small} alt={single?.alt__description} />
                 <div>
                     <h3>{single?.user?.first_name}</h3>
-                    <p>Made to Change</p>
+                    <p>{single?.sponsorship?.tagline}</p>
                 </div>
             </div>
             <div className="single__card-right">
                 <button className="right__btn"><FaHeart/></button>
                 <button className="right__btn"><FaPlus/></button>
-                <button className="single__download">
+                <button className="single__download" onClick={()=>downloadImage(single)}>
                     <p>Download</p>
                     <IoIosArrowDown />
                 </button>
             </div>
         </div>
-        <img src={single?.urls?.raw} alt="img" />
+        <img className="img" src={single?.urls?.raw} alt="img" />
+        <SinglePhotos photos={photos} downloadImage={downloadImage}/>
       </div>
       <div className="slider__btns">
-        <button className="prev"><GrPrevious/></button>
-        <button className="next"><GrNext/></button>
+        <button className="prev" disabled={index === 0 ? true : false} onClick={()=>setPropIndex(prev=> prev + 1)}><GrPrevious/></button>
+        <button className="next" onClick={()=>setPropIndex(prev=> prev + 1)}><GrNext/></button>
       </div>
-      {/* <Pictures photos={photos}/> */}
+      
     </div>
   );
 };
--- src/components/SinglePicture/Singlephotos/SinglePhotos.jsx
+++ src/components/SinglePicture/Singlephotos/SinglePhotos.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
-import "./Pictures.scss";
-import { FaHeart } from "react-icons/fa";
-import { FaArrowDown, FaPlus } from "react-icons/fa6";
-import SinglePicture from "../SinglePicture/SinglePicture";
+import React from 'react'
+import { FaArrowDown, FaHeart, FaPlus } from 'react-icons/fa';
 
-const Pictures = ({photos}) => {
-  let num = photos.length;
+const SinglePhotos = ({photos, downloadImage}) => {
+    let num = photos.length;
   let length1 = parseInt(num / 3);
   let length2 = parseInt((num / 3) * 2);
   let photosFilter1 = photos.filter((item, index) => index < length1);
@@ -13,50 +10,23 @@ const Pictures = ({photos}) => {
     (item, index) => index < length2 && length1 <= index
   );
   let photosFilter3 = photos.filter((item, index) => index >= length2);
-  const [heartId, setHeartId] = useState("")
-  const downloadImage =(item)=> {
-    setSinglePictureStatus(false)
-    const imageUrl = item.urls.raw
-    console.log(item);
-    fetch(imageUrl)
-      .then(response => response.blob())
-      .then(blob => {
-        const url = window.URL.createObjectURL(new Blob([blob]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', `${item.slug}.jpg`); 
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      })
-      .catch(error => {
-        console.error('Error downloading the image: ', error);
-      });
-  }
-  const [propIndex, setPropIndex] = useState(0)
-  const [singlePictureStatus, setSinglePictureStatus] = useState(false)
-  const singlePhoto =(index)=> {
-    setPropIndex(index)
-    setSinglePictureStatus(true)
-  }
   return (
-    <div className="pictures">
-      <SinglePicture downloadImage={downloadImage} index={propIndex} setPropIndex={setPropIndex} photos={photos} setSinglePictureStatus={setSinglePictureStatus} singlePictureStatus={singlePictureStatus}/>
-      <div className="pictures__cards">
-        <div className="div">
-          {photosFilter1?.map((item, index) => {
+  <div className="pictures">
+     <div className='pictures__cards'>
+     <div className='div'>
+      {photosFilter1?.map((item, index) => {
             return (
               <div key={index} className="pictures__card relative">
                 <img
                   src={item?.urls?.raw}
                   alt="pictures"
-                  className=" max-w-[100%]"
+                  className="w-[100%]"
                 />
-                <div className="picture__card-top" onClick={()=>singlePhoto(index)}>
+                <div className="picture__card-top">
                   <div className="card_item">
                     <p className="text-[14px] text-white">Sponsored</p>
                     <div className="flex gap-[10px]">
-                    <button onClick={()=>setHeartId(prev=> prev != item.id + "a" ? prev = item.id + "a" : prev = "")} className={`px-[15px] py-[10px] rounded-md cursor-pointer ${heartId === item.id + "a" ? "bg-red-500 text-white opacity-[1]" : "bg-white "} pictures__btn`}>
+                    <button  className={`px-[15px] py-[10px] rounded-md cursor-pointer bg-white pictures__btn`}>
                         <FaHeart />
                         </button>
                       <button className="bg-white px-[15px] py-[10px] rounded-md cursor-pointer pictures__btn ">
@@ -77,80 +47,80 @@ const Pictures = ({photos}) => {
               </div>
             );
           })}
-        </div>
-        <div className="div">
-          {photosFilter2?.map((item, index) => {
-            return (
-              <div key={index} className="pictures__card relative">
-                <img
-                  src={item?.urls?.raw}
-                  alt="pictures"
-                  className=" max-w-[100%]"
-                />
-                <div className="picture__card-top" onClick={()=>singlePhoto(index)}>
-                  <div className="card_item">
-                    <p className="text-[14px] text-white">Sponsored</p>
-                    <div className="flex gap-[10px]">
-                    <button onClick={()=>setHeartId(prev=> prev != item.id + "a" ? prev = item.id + "a" : prev = "")} className={`px-[15px] py-[10px] rounded-md cursor-pointer ${heartId === item.id + "a" ? "bg-red-500 text-white opacity-[1]" : "bg-white "} pictures__btn`}>
-                        <FaHeart />
-                        </button>
-                      <button className="bg-white px-[15px] py-[10px] rounded-md cursor-pointer pictures__btn ">
-                      <FaPlus />
-                      </button>
-                    </div>
-                  </div>
-                <div className="w-[100%] flex justify-between items-center ">
-                  <div className="flex gap-[5px] items-center profile__img">
-                    <img src={item?.user?.profile_image?.small} alt="profile" className="rounded-full" />
-                    <h1 className="text-white">{item?.user?.first_name}</h1>
-                  </div>
-                  <button onClick={()=>downloadImage(item)} className="bg-white rounded-md cursor-pointer pictures__btn download__btn">
-                    <p>Download</p>
-                    <FaArrowDown className="download__icon"/></button>
-                </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-        <div className="div">
-          {photosFilter3?.map((item, index) => {
-            return (
-              <div key={index} className="pictures__card relative">
-                <img
-                  src={item?.urls?.raw}
-                  alt="pictures"
-                  className=" max-w-[100%]"
-                />
-                <div className="picture__card-top" onClick={()=>singlePhoto(index)}>
-                  <div className="card_item">
-                    <p className="text-[14px] text-white">Sponsored</p>
-                    <div className="flex gap-[10px]">
-                    <button onClick={()=>setHeartId(prev=> prev != item.id + "a" ? prev = item.id + "a" : prev = "")} className={`px-[15px] py-[10px] rounded-md cursor-pointer ${heartId === item.id + "a" ? "bg-red-500 text-white opacity-[1]" : "bg-white "} pictures__btn`}>
-                        <FaHeart />
-                        </button>
-                      <button className="bg-white px-[15px] py-[10px] rounded-md cursor-pointer pictures__btn ">
-                      <FaPlus />
-                      </button>
-                    </div>
-                  </div>
-                <div className="w-[100%] flex justify-between items-center ">
-                  <div className="flex gap-[5px] items-center profile__img">
-                    <img src={item?.user?.profile_image?.small} alt="profile" className="rounded-full" />
-                    <h1 className="text-white">{item?.user?.first_name}</h1>
-                  </div>
-                  <button onClick={()=>downloadImage(item)} className="bg-white rounded-md cursor-pointer pictures__btn download__btn">
-                    <p>Download</p>
-                    <FaArrowDown className="download__icon"/></button>
-                </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
     </div>
-  );
-};
+    <div className='div'>
+      {photosFilter2?.map((item, index) => {
+            return (
+              <div key={index} className="pictures__card relative">
+                <img
+                  src={item?.urls?.raw}
+                  alt="pictures"
+                  className="w-[100%]"
+                />
+                <div className="picture__card-top">
+                  <div className="card_item">
+                    <p className="text-[14px] text-white">Sponsored</p>
+                    <div className="flex gap-[10px]">
+                    <button  className={`px-[15px] py-[10px] rounded-md cursor-pointer bg-white pictures__btn`}>
+                        <FaHeart />
+                        </button>
+                      <button className="bg-white px-[15px] py-[10px] rounded-md cursor-pointer pictures__btn ">
+                      <FaPlus />
+                      </button>
+                    </div>
+                  </div>
+                <div className="w-[100%] flex justify-between items-center ">
+                  <div className="flex gap-[5px] items-center profile__img">
+                    <img src={item?.user?.profile_image?.small} alt="profile" className="rounded-full" />
+                    <h1 className="text-white">{item?.user?.first_name}</h1>
+                  </div>
+                  <button onClick={()=>downloadImage(item)} className="bg-white rounded-md cursor-pointer pictures__btn download__btn">
+                    <p>Download</p>
+                    <FaArrowDown className="download__icon"/></button>
+                </div>
+                </div>
+              </div>
+            );
+          })}
+    </div>
+    <div className='div'>
+      {photosFilter3?.map((item, index) => {
+            return (
+              <div key={index} className="pictures__card relative">
+                <img
+                  src={item?.urls?.raw}
+                  alt="pictures"
+                  className="w-[100%]"
+                />
+                <div className="picture__card-top">
+                  <div className="card_item">
+                    <p className="text-[14px] text-white">Sponsored</p>
+                    <div className="flex gap-[10px]">
+                    <button  className={`px-[15px] py-[10px] rounded-md cursor-pointer bg-white pictures__btn`}>
+                        <FaHeart />
+                        </button>
+                      <button className="bg-white px-[15px] py-[10px] rounded-md cursor-pointer pictures__btn ">
+                      <FaPlus />
+                      </button>
+                    </div>
+                  </div>
+                <div className="w-[100%] flex justify-between items-center ">
+                  <div className="flex gap-[5px] items-center profile__img">
+                    <img src={item?.user?.profile_image?.small} alt="profile" className="rounded-full" />
+                    <h1 className="text-white">{item?.user?.first_name}</h1>
+                  </div>
+                  <button onClick={()=>downloadImage(item)} className="bg-white rounded-md cursor-pointer pictures__btn download__btn">
+                    <p>Download</p>
+                    <FaArrowDown className="download__icon"/></button>
+                </div>
+                </div>
+              </div>
+            );
+          })}
+    </div>
+   </div>
+  </div>
+  )
+}
 
-export default Pictures;
+export default SinglePhotos
